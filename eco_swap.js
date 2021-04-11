28c001a0540ea295263159dc1c1e4f7986e4963a
libii <libii@gmail.com>
2021-04-10 23:55:41 -0700

    Changes

--- eco_swap.js
+++ eco_swap.js
@@ -144,32 +144,72 @@ if (sustainable == true) {
   myList.classList.add("myList");
   popupElement.appendChild(myList);
 
-  //make table
-  var myTable = document.createElement("table");
-  myList.appendChild(myTable);
+  // //make table
+  // var myTable = document.createElement("table");
+  // myList.appendChild(myTable);
 
-  var myRow = document.createElement("tr");
-  myTable.appendChild(myRow);
+  // var myRow = document.createElement("tr");
+  // myTable.appendChild(myRow);
 
-  var myData = document.createElement("td");
-  myRow.appendChild(myData);
+  // var myData = document.createElement("td");
+  // myRow.appendChild(myData);
 
-  var myImage = document.createElement("img");
-  myImage.src = "https://lh3.ggpht.com/hWCPw3isbmqesoj5e4Td8qfCU_ELq6nkJAuvi79EEtzhKQFI4iwAzJdgXE8w58o88w=w170";
-  myImage.style = "width: 50px; height:48px";
-  myData.appendChild(myImage);
+  // var myImage = document.createElement("img");
+  // myImage.src = "https://lh3.ggpht.com/hWCPw3isbmqesoj5e4Td8qfCU_ELq6nkJAuvi79EEtzhKQFI4iwAzJdgXE8w58o88w=w170";
+  // myImage.style = "width: 50px; height:48px";
+  // myData.appendChild(myImage);
 
-  myData = document.createElement("td");
-  myData.textContent = "Product Name";
-  myRow.appendChild(myData);
+  // myData = document.createElement("td");
+  // myData.textContent = "Product Name";
+  // myRow.appendChild(myData);
 
-  myData = document.createElement("td");
-  myData.textContent = "$" + 11.11;
-  myRow.appendChild(myData);
+  // myData = document.createElement("td");
+  // myData.textContent = "$" + 11.11;
+  // myRow.appendChild(myData);
 
-  myData = document.createElement("td");
-  myData.textContent = "label 1, label 2";
-  myRow.appendChild(myData);
+  // myData = document.createElement("td");
+  // myData.textContent = "label 1, label 2";
+  // myRow.appendChild(myData);
+
+  fetch('data/eco_list.json')
+  .then(response => response.json())
+  .then(data => {console.log(data);
+    console.log(data[0].price);
+    console.log(Object.keys(data).length); //length
+    for(var k = 0; k < Object.keys(data).length-1; ++k){
+      //row
+      var myRow = document.createElement("tr");
+      myTable.appendChild(myRow);
+
+      var myData = document.createElement("td");
+      myRow.appendChild(myData);
+    
+      var myImage = document.createElement("img");
+      myImage.src = "images/" +data[k].images;
+      myImage.style = "width: 50px; height:48px";
+      myData.appendChild(myImage);
+    
+      myData = document.createElement("td");
+      var myLink = document.createElement("a");
+      //myData.textContent = data[k].swaps;
+      myRow.appendChild(myData);
+
+      var myLink = document.createElement("a");
+      myLink.href = data[k].link;
+      myLink.target="_blank";
+      //mylink.innerText = data[k].swaps;
+      myData.appendChild(myLink);
+    
+      myData = document.createElement("td");
+      myData.textContent = "$" + data[k].price;
+      myRow.appendChild(myData);
+    
+      myData = document.createElement("td");
+      myData.textContent = "label 1, label 2";
+      myRow.appendChild(myData);
+
+    }
+  });
 
 }
 
